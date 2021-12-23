dbc2483c0cb639f6d8f2c5cb00ca0544926debf7
Denis Tofoli <denistofoli@users.noreply.github.com>
2021-12-23 15:20:12 -0300

    Add files via upload

---
+++ advpl-build-bot.py
@@ -0,0 +1,93 @@
+import subprocess
+import os
+from datetime import datetime
+
+
+BUILD_PATH = '/home/project/protheus'  # Allways Full Path
+BUILD_BRANCH = 'master'
+BUILD_INTERVAL = '10.day'  # see https://git-scm.com/docs/git-log
+BUILD_INCLUDES = '/home/Protheus/include'  # Allways Full Path
+BUILD_SERVER = '127.0.0.1'
+BUILD_PORT = '1234'
+BUILD_ENV = 'Tests'
+BUILD_USER = 'admin'
+BUILD_PASS = ''
+BUILD_FILES = ''
+BUILD_LOGS = []
+
+
+def git_pull():
+    list_files = ['git', 'pull']
+    linhas = subprocess.run(list_files, stdout=subprocess.PIPE,
+                            cwd=BUILD_PATH).stdout.decode('UTF-8').__str__().split('\n')
+    add_log('GIT', linhas)
+
+
+def load_changed_files():
+    files = ''
+    last_update = ['git', 'log', BUILD_BRANCH, '--since=' + BUILD_INTERVAL,
+                   '--name-only', '--line-prefix=' + BUILD_PATH + '/']
+    linhas = subprocess.run(last_update, stdout=subprocess.PIPE,
+                            cwd=BUILD_PATH).stdout.decode('UTF-8').__str__().split('\n')
+
+    for x in linhas:
+        if '.prw' in x.lower() and x not in files:
+            files += ',' if files.__len__() > 0 else ''
+            files += x
+
+    return files
+
+
+def make_ini():
+    f = open("build-bot.ini", "w")
+
+    f.write('showConsoleOutput=true\n')
+
+    f.write('[authentication]\n')
+    f.write('action=authentication\n')
+    f.write('server=' + BUILD_SERVER + '\n')
+    f.write('port=' + BUILD_PORT + '\n')
+    f.write('secure=0\n')
+    f.write('build=AUTO\n')
+    f.write('environment=' + BUILD_ENV + '\n')
+    f.write('user=admin\n')
+    f.write('psw=\n')
+
+    f.write('[compile]\n')
+    f.write('action=compile\n')
+    f.write('program=' + BUILD_FILES + '\n')
+    f.write('recompile=T\n')
+    f.write('includes=' + BUILD_INCLUDES + '\n')
+
+    f.close()
+
+
+def make_build():
+    if BUILD_FILES.strip().__len__() > 0:
+        make = [os.getcwd() + '/advpls', '--tdsCli=build-bot.ini']
+        linhas = subprocess.run(make, stdout=subprocess.PIPE,
+                                cwd=os.getcwd()).stdout.decode('UTF-8').__str__().split('\n')
+        add_log('BUILD', linhas)
+    else:
+        add_log('BUILD', ['No files to compile'])
+
+
+def add_log(prefix, linhas):
+    for linha in linhas:
+        if linha.strip().__len__() > 0:
+            BUILD_LOGS.append('[' + datetime.now().__str__() + ']' + '[' + prefix + '] ' + linha + '\n')
+
+
+def write_log():
+    f = open("build-bot.log", "w")
+    for x in BUILD_LOGS:
+        f.write(x)
+    f.close()
+
+
+if __name__ == '__main__':
+    git_pull()
+    BUILD_FILES = load_changed_files()
+    make_ini()
+    make_build()
+    write_log()
