85434931ba9fde6be8e293ccfc07bd70eb499d10
Alexandre Florentino <kanemaki@gmail.com>
2021-12-30 01:38:31 +0000

    feat: add class config and file json

---
+++ build_config.py
@@ -0,0 +1,89 @@
+#from jsonschema import validate
+import json
+import os.path
+
+
+class Build_Config:
+
+    schema = {                
+                "type": "object",
+                "properties": {
+                    "configs": {
+                    "type": "array",
+                    "items": [
+                        {
+                        "type": "object",
+                        "properties": {
+                            "totvsServer": {
+                            "type": "object",
+                            "properties": {
+                                "path": {
+                                "type": "string"
+                                },
+                                "includes": {
+                                "type": "string"
+                                },
+                                "server": {
+                                "type": "string"
+                                },
+                                "port": {
+                                "type": "string"
+                                },
+                                "env": {
+                                "type": "string"
+                                },
+                                "user": {
+                                "type": "string"
+                                },
+                                "pass": {
+                                "type": "string"
+                                }
+                            },
+                            "required": [
+                                "path",
+                                "includes",
+                                "server",
+                                "port",
+                                "env",
+                                "user",
+                                "pass"
+                            ]
+                            },
+                            "git": {
+                            "type": "object",
+                            "properties": {
+                                "branch": {
+                                "type": "string"
+                                },
+                                "internal": {
+                                "type": "string"
+                                }
+                            },
+                            "required": [
+                                "branch",
+                                "internal"
+                            ]
+                            }
+                        },
+                        "required": [
+                            "totvsServer",
+                            "git"
+                        ]
+                        }
+                    ]
+                    }
+                },
+                "required": [
+                    "configs"
+                ]
+            }
+    
+    def read_json(arq_json):
+        control = os.path.isfile(arq_json)    
+        if control:
+            with open(arq_json, 'r', encoding='utf8') as f:
+                return json.load(f)
+
+    #data = ler_json('build-bot-config.json')
+    #validate(data, schema)
+    #print(data["configs"][0]["totvsServer"]["path"])
