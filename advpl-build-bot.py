17f9423fc7d17f29674ad61c4ab2963fc229acac
Denis Tofoli <denis.tofoli@gmail.com>
2022-01-01 16:40:04 -0300

    feat: add configs iterator

--- advpl-build-bot.py
+++ advpl-build-bot.py
@@ -5,34 +5,31 @@ from build_advpl import Build_Advpl
 from build_config import Build_Config
 
 
-configs = Build_Config("build-bot-config.json")
+def main():
+    log = Build_Log()
 
-# Allways Full Path
-BUILD_PATH = configs.configs[0].path  # (For Windows use '\\' like c:\\totvs\\sources)
-BUILD_INCLUDES = configs.configs[0].includes 
+    configs = Build_Config("build-bot-config.json")
+    log.add('CONFIG', [configs.message])
 
-# Git
-BUILD_BRANCH = configs.configs[0].branch
-BUILD_INTERVAL = configs.configs[0].interval # see https://git-scm.com/docs/git-log
+    for config in configs.configs:
+        system = Build_System(config.path)
+        git = Build_Git(system, config.branch, config.interval)
+        advpl = Build_Advpl(system,
+                            config.server,
+                            config.port,
+                            config.env,
+                            config.includes,
+                            config.user,
+                            config.passwd)
 
-# AppServer
-BUILD_SERVER = configs.configs[0].server 
-BUILD_PORT = configs.configs[0].port 
-BUILD_ENV = configs.configs[0].env 
-BUILD_USER = configs.configs[0].user 
-BUILD_PASS = configs.configs[0].passwd 
+        log.add('PROJECT', [config.name])
+        log.add(system.os, ['Starting Advpl Build Bot'])
+        log.add('GIT', git.pull())
+        log.add('BUILD',advpl.build(git.get_changed_files()))
+        log.add(system.os, ['Finish'])
 
-
-# Internal script usage
-log = Build_Log()
-system = Build_System(BUILD_PATH)
-git = Build_Git(system, BUILD_BRANCH, BUILD_INTERVAL)
-advpl = Build_Advpl(system,  BUILD_SERVER, BUILD_PORT, BUILD_ENV, BUILD_INCLUDES, BUILD_USER, BUILD_PASS)
+    log.write_log()
 
 
 if __name__ == '__main__':
-    log.add(system.os, ['Starting Advpl Build Bot'])
-    log.add('GIT', git.pull())
-    log.add('BUILD',advpl.build(git.get_changed_files()))
-    log.add(system.os, ['Finish job'])
-    log.write_log()
+    main()
