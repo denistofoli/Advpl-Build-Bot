5a2321e5c17e8ac1096dc56761e208c92ae7d6ce
Denis Tofoli <denis@lexer.com.br>
2025-12-02 12:14:05 -0300

    feat: args to select environment

--- advpl-build-bot.py
+++ advpl-build-bot.py
@@ -1,3 +1,4 @@
+import sys
 from build_log import Build_Log
 from build_system import Build_System
 from build_git import Build_Git
@@ -12,22 +13,23 @@ def main():
     log.add('CONFIG', [configs.message])
 
     for config in configs.configs:
-        system = Build_System(config.path)
-        git = Build_Git(system, config.branch, config.interval, config.show_type)
-        advpl = Build_Advpl(system,
-                            config.server,
-                            config.port,
-                            config.env,
-                            config.includes,
-                            config.user,
-                            config.passwd)
+        if not sys.argv[1] or config.name == sys.argv[1]:
+            system = Build_System(config.path)
+            git = Build_Git(system, config.branch, config.interval, config.show_type)
+            advpl = Build_Advpl(system,
+                                config.server,
+                                config.port,
+                                config.env,
+                                config.includes,
+                                config.user,
+                                config.passwd)
 
-        log.add('PROJECT', [config.name])
-        log.add(system.os, ['Starting Advpl Build Bot'])
-        log.add('GIT CHECKOUT', git.checkout())
-        log.add('GIT PULL', git.pull())
-        log.add('BUILD',advpl.build(git.get_changed_files()))
-        log.add(system.os, ['Finish'])
+            log.add('PROJECT', [config.name])
+            log.add(system.os, ['Starting Advpl Build Bot'])
+            log.add('GIT CHECKOUT', git.checkout())
+            log.add('GIT PULL', git.pull())
+            log.add('BUILD',advpl.build(git.get_changed_files()))
+            log.add(system.os, ['Finish'])
 
     log.write_log()
 
